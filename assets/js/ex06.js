30978c90d228dbbf1d1bfa81c0acb721c333db16
matheus0pestana@gmail.com <matheus0pestana@gmail.com>
2025-04-03 18:00:25 -0300

    .

--- assets/js/ex06.js
+++ assets/js/ex06.js
@@ -1,7 +1,46 @@
 var nomes = []
+var lista = document.getElementById("lista");
 
-function ordenar() {
-    var input = document.getElementById("nomes").value;
-    input = nomes
-    console.log(nomes)
+function adiciona() {
+
+    var inputElement = document.getElementById("nomes");
+    var inputValue = inputElement.value
+
+    if (nomes.length >= 5) {
+        alert("Limite de 5 nomes atingido.");
+        return;
+    } else if (inputValue == "") {
+        alert("Campo vazio, digite um nome.");
+        return;
+    } else {
+        nomes.push(inputValue);
+        inputElement.value = "";
+        console.log(nomes);
+    }
+
+    if (nomes.length == 5) {
+        alert("Limite de 5 nomes atingido.");
+        document.getElementById("asc").disabled = false;
+        document.getElementById("desc").disabled = false;
+    }
+}
+
+function asc() {
+    nomes.sort();
+    lista.innerHTML = "";
+    for (let i = 0, j = nomes.length; i < j; i++) {
+        nomes[i] = nomes[i].toLowerCase();
+        console.log(nomes[i]);
+        lista.innerHTML += "<li>" + nomes[i] + "</li>";
+    }
+}
+
+function desc() {
+    nomes.sort().reverse();
+    lista.innerHTML = "";
+    for (let i = 0, j = nomes.length; i < j; i++) {
+        nomes[i] = nomes[i].toUpperCase();
+        console.log(nomes[i]);
+        lista.innerHTML += "<li>" + nomes[i] + "</li>";
+    }
 }
